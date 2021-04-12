3c70ebd4a5d9ebdacc7c371659625e08bf4d1bcf
= <=>
2021-04-12 17:04:31 +0700

    Membuat Komponen Dasar

--- App.js
+++ App.js
@@ -1,106 +1,26 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {Image, Text, TextInput, View} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+const App = () => {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View>
+      <View style={{width:80,height:80,backgroundColor:'#0adbe3'}}/>
+      <Text>TB CCCP</Text>
+      <Text>POCCNR 1</Text>
+      <Lokal/>
+      <Text>QAZAQ TV</Text>
+      <Photo/>
+      <TextInput style={{borderWidth:1}}/>
     </View>
   );
 };
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+const Lokal = () => {
+  return <Text>NET TV</Text>
+}
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Aplikasi Pertama">
-            Ini adalah <Text style={styles.highlight}>Aplikasi Pertama</Text> yang saya buat dalam android .
-            Anda bisa melihatnya hari ini di status WA saya , saya menggunakan react native dalam pembuatan Aplikasi
-            Ini , Selamat Malam 
-          </Section>
-          <Section title="Pelajari Lebih Lanjut">
-            Read the docs to discover what to do next:
-          </Section>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+const Photo = () => {
+  return <Image source={{uri:'https://placeimg.com/100/100/tech'}} style={{width:100,height:100}}/>
+}
 
 export default App;
